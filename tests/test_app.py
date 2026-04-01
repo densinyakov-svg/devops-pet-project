f117a7e176d7a2076f38a14f33e6d9dcea38ae37
unknown <densinyako@gmail.com>
2026-04-01 22:47:54 +0300

    initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
